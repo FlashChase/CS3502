5dfbdb713142eece84483beb38442bb517db6a98
Chase <cfcconnell@gmail.com>
2026-01-29 16:51:49 -0600

    Modified consumer.c and producer.c with better comments

--- assignment1/c-programs/consumer.c
+++ assignment1/c-programs/consumer.c
@@ -6,18 +6,22 @@
 #include <signal.h>
 #include <unistd.h>
 
-volatile sig_atomic_t shutdown_flag = 0;
-volatile sig_atomic_t sigusr1_flag = 0;
+volatile sig_atomic_t shutdown_flag = 0;	// Used to flag SIGINT
+volatile sig_atomic_t sigusr1_flag = 0;		// Used to flag SIGUSR1
 
+// Handles SIGINT and SIGUSR1 Signals
 void handle_sigint(int sig) {
-    if (sig == SIGINT) {
-       // char sig_message[] = " User initiated shutdown...\n";
-       // write(STDERR_FILENO, sig_message, sizeof(sig_message) - 1);
-        shutdown_flag = 1;
-    }
-    else if (sig == SIGUSR1) {
-        sigusr1_flag = 1;
-    }
+	// IF SIGINT signal received mark shutdown flag & exit. (Output message already sent from producer)
+	if (sig == SIGINT) {
+		// char sig_message[] = " User initiated shutdown...\n";
+		// write(STDERR_FILENO, sig_message, sizeof(sig_message) - 1);
+		shutdown_flag = 1;
+		exit(1);
+	}
+	// IF SIGUSR1 signal received, flag it
+	else if (sig == SIGUSR1) {
+	sigusr1_flag = 1;
+	}
 }
 
 
@@ -33,6 +37,13 @@ int main(int argc, char *argv[]) {
 	ssize_t line_bytes;         // Stores number of characters (bytes) on each line
 	char *line = NULL;          // Pointer for getline()
 	size_t buf = 0;             // Allocate memory for getline()
+
+	struct sigaction sa;
+	sa.sa_handler = handle_sigint;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;	// Had to change this from 0 to SA_RESTART to reliably call SIGUSR1 multiple times
+	sigaction(SIGINT, &sa, NULL);	// Send SIGINT to sig handler method
+	sigaction(SIGUSR1, &sa, NULL);	// Send SIGUSR1 to sig handler method
 
 	while ((opt = getopt(argc, argv, "n:v")) != -1) {
 		switch (opt) {
@@ -50,14 +61,9 @@ int main(int argc, char *argv[]) {
 		} // End switch
 	} // End while
 
-	struct sigaction sa;
-	sa.sa_handler = handle_sigint;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-	sigaction(SIGINT, &sa, NULL);
-	sigaction(SIGUSR1, &sa, NULL);
-
+	// While SIGINT signal not received and the sentinel has not been flagged
 	while (!shutdown_flag && !sentinel) {
+		// If SIGUSR1 is flagged, reset flag, Print line and character count
 		if (sigusr1_flag) {
 			sigusr1_flag = 0;
 			fprintf(stderr, "Lines: %d | Characters: %d\n", line_count, char_count);
@@ -65,7 +71,7 @@ int main(int argc, char *argv[]) {
 
 		if (is_line_limit) {                    // If a line limit exists
 			if (line_count >= line_limit - 1) { // If line counter equals line limit
-				sentinel = 1;                   // Line limit flag sentinel
+				sentinel = 1;                   // Line limit reached, flag sentinel
 			}
 		}
 
@@ -81,8 +87,10 @@ int main(int argc, char *argv[]) {
 				printf("%s", line);             // If verbose option entered
 			}
 		}
+		// If stdin is empty, flag sentinel, print final line and character count
+		// Count includes newline characters. Must subtract line_count from char_count if you don't want them included
 		else {
-			sentinel = 1;                       // If stdin is empty
+			sentinel = 1;
 			fprintf(stderr, "Lines: %d | Characters: %d\n", line_count, char_count);
 		}
 	} // End sentinel loop
--- assignment1/c-programs/producer.c
+++ assignment1/c-programs/producer.c
@@ -6,42 +6,47 @@
 #include <signal.h>
 #include <unistd.h>
 
-volatile sig_atomic_t shutdown_flag = 0;
-volatile sig_atomic_t sigusr1_flag = 0;
+volatile sig_atomic_t shutdown_flag = 0;	// Used to flag SIGINT
+volatile sig_atomic_t sigusr1_flag = 0;		// Used to flag SIGUSR1
 
+// Handles SIGINT and SIGUSR1 Signals
 void handle_sigint(int sig) {
-    if (sig == SIGINT) {
-        char sig_message[] = " User initiated shutdown...\n";
-        write(STDERR_FILENO, sig_message, sizeof(sig_message)-1);
-        shutdown_flag = 1;
-    }
-    else if (sig == SIGUSR1) {
-        sigusr1_flag = 1;
-    }
+
+	// If SIGINT signal sent, Output custom message to stderr, mark shutdown flag, and exit.
+	if (sig == SIGINT) {
+		char sig_message[] = " User initiated shutdown...\n";
+		write(STDERR_FILENO, sig_message, sizeof(sig_message)-1);
+		shutdown_flag = 1;
+		exit(1);
+	}
+	// If SIGUSR1 signal received, flag it.
+	else if (sig == SIGUSR1) {
+	sigusr1_flag = 1;
+	}
 }
 
 int main(int argc, char *argv[]) {
 	FILE *read_file;
 	int opt;
-	int buffer_size = 4096;
+	int buffer_size = 4096;	// Default buffer size
 	char *filename;
-	int filename_input = 0;
+	int filename_input = 0;	// Flag used for if filename is entered or not
 
 	struct sigaction sa;
 	sa.sa_handler = handle_sigint;
 	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-	sigaction(SIGINT, &sa, NULL);
-	sigaction(SIGUSR1, &sa, NULL);
+	sa.sa_flags = SA_RESTART;	// Had to change this from 0 to SA_RESTART to reliably use SIGUSR1
+	sigaction(SIGINT, &sa, NULL);	// Send SIGINT to sig handler method
+	sigaction(SIGUSR1, &sa, NULL);	// Send SIGUSR1 to sig handler method
 
 	while ((opt = getopt(argc, argv, "f:b:")) != -1) {
         	switch (opt) {
             		case 'f':
                 		filename = optarg; // optarg contains the argument value
-                		filename_input = 1;
+                		filename_input = 1;// flag as file name entered
                 		break;
             		case 'b':
-                		buffer_size = atoi(optarg);
+                		buffer_size = atoi(optarg); // Save user input buffer size
 				break;
 			default:
                 		fprintf(stderr, "Usage: %s [-f file] [-b size]\n", argv[0]);
@@ -49,28 +54,36 @@ int main(int argc, char *argv[]) {
 		} // End switch
 	} // End getopt while
 
+	// While loop is really just for testing SIGINT & SIGUSR1. Comment out the return to get an infinite loop until SIGINT
 	while (!shutdown_flag) {
+
+		// Test if SIGUSR1 is flagged
 		if (sigusr1_flag) {
-			kill(-getpgrp(), SIGUSR1);
-			sigusr1_flag = 0;
+			kill(-getpgrp(), SIGUSR1);	// Send SIGUSR1 signal to process group (to pass to child)
+			sigusr1_flag = 0;		// Reset SIGUSR1 flag
 		}
 
-		char buffer[buffer_size];
+		char buffer[buffer_size];	// Create buffer using default or user input buffer size
 
+		// If user input a file name
 		if (filename_input) {
+			// Open file in read mode
 			read_file = fopen(filename, "r");
 
+			// If read fails, Print error message, flag as no filename entered
 			if (read_file == NULL) {
 				printf("Error reading from file\n");
 				filename_input = -1;
 			}
+			// If file open was successful
 			else {
+				// Read buffer sized sections and print to stdout until eof
 				while ((fgets(buffer, sizeof(buffer), read_file)) != NULL) {
 					fprintf(stdout, "%s", buffer);
 				}
 			}
 		}
-
+		// If no filename input, allow manual input
 		if (!filename_input) {
 			printf("Enter Input: ");
 			while ((fgets(buffer, sizeof(buffer), read_file)) != NULL) {
