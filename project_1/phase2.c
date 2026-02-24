d7ce6b8ec9943ae38743099653281bca9d3c2f6f
Chase <cfcconnell@gmail.com>
2026-02-23 18:22:48 -0600

    Update phase2.c to expected final version

--- project_1/phase2.c
+++ project_1/phase2.c
@@ -26,9 +26,6 @@ typedef struct {
 // Global shared array - THIS CAUSES RACE CONDITIONS !
 Account accounts[NUM_ACCOUNTS];
 
-// Set global variable to track totals of all accounts. Also causes race conditions.
-double expected_total = NUM_ACCOUNTS * INITIAL_BALANCE;
-
 // GIVEN : Example deposit function WITH race condition
 void deposit_safe ( int account_id , double amount ) {
 	// Acquire lock BEFORE accessing shared data
@@ -38,7 +35,6 @@ void deposit_safe ( int account_id , double amount ) {
 	// Only ONE thread can execute this at a time for this account
 	accounts[account_id].balance += amount;
 	accounts[account_id].transaction_count++;
-	expected_total += amount;
 	// ============================
 
 	// Release lock AFTER modifying shared data
@@ -46,20 +42,17 @@ void deposit_safe ( int account_id , double amount ) {
 }
 
 // TODO 1: Implement withdrawal_safe () with mutex protection
-// Reference : Follow the pattern of deposit_safe () above
-// Remember : lock BEFORE accessing data , unlock AFTER
 void withdrawal_safe ( int account_id , double amount ) {
-	// YOUR CODE HERE
+
 	// Hint : pthread_mutex_lock
 	// Hint : Modify balance
 	// Hint : pthread_mutex_unlock
-	pthread_mutex_lock(&accounts[account_id].lock);
+	pthread_mutex_lock(&accounts[account_id].lock);		// Acquire lock
 
-	accounts[account_id].balance -= amount;
-	accounts[account_id].transaction_count++;
-	expected_total -= amount;
+	accounts[account_id].balance -= amount;			// Update balance
+	accounts[account_id].transaction_count++;		// Update transaction count
 
-	pthread_mutex_unlock(&accounts[account_id].lock);
+	pthread_mutex_unlock(&accounts[account_id].lock);	// Release lock
 }
 
 
@@ -69,41 +62,34 @@ void withdrawal_safe ( int account_id , double amount ) {
 void* teller_thread(void* arg) {
 	int teller_id = *(int*) arg ; // GIVEN : Extract thread ID
 
-	// TODO 2 a : Initialize thread - safe random seed
-	// Reference : Section 7.2 " Random Numbers per Thread "
-	// Hint : unsigned int seed = time ( NULL ) ^ pthread_self () ;
-
-	unsigned int seed = time(NULL)^pthread_self();
+	unsigned int seed = time(NULL)^pthread_self();			// Creat seed for random
 
 	for ( int i = 0; i < TRANSACTIONS_PER_THREAD ; i ++) {
-		// TODO 2 b : Randomly select an account (0 to NUM_ACCOUNTS -1)
-		// Hint : Use rand_r (& seed ) % NUM_ACCOUNTS
-		int account_idx = rand_r(&seed) % NUM_ACCOUNTS;
 
-		// TODO 2 c : Generate random amount (1 -100)
-		double amount = rand_r(&seed) % 100 + 1;
+		int account_idx = rand_r(&seed) % NUM_ACCOUNTS;		// Determine account 1
+		int account_idx2 = rand_r(&seed) % NUM_ACCOUNTS;	// Determine account 2
 
-		// TODO 2 d : Randomly choose deposit (1) or withdrawal (0)
-		// Hint : rand_r (& seed ) % 2
-		int operation = rand_r(&seed) % 2;
+		double amount = rand_r(&seed) % 100 + 1;		// Determine transaction amount
 
-		// TODO 2 e : Call appropriate function
-		if ( operation == 1) {
-			deposit_safe ( account_idx , amount ) ;
-			printf ("Teller %d: Deposited $%.2f to Account %d \n" ,
-				teller_id , amount , account_idx ) ;
-		} else {
-			// YOUR CODE HERE - call withdrawal_unsafe
-			withdrawal_safe ( account_idx , amount ) ;
-			printf ("Teller %d: Withdrew $%.2f to Account %d \n" ,
-				teller_id , amount , account_idx);
-		}
+		// int operation = rand_r(&seed) % 2;	// Not needed for transfers
+
+		// Call updated deposit method
+		deposit_safe ( account_idx , amount ) ;
+		printf ("Teller %d: Deposited $%.2f to Account %d \n" ,
+			teller_id , amount , account_idx ) ;
+
+		// Call updated withdrawal method
+		withdrawal_safe ( account_idx2 , amount ) ;
+		printf ("Teller %d: Withdrew $%.2f from Account %d \n" ,
+			teller_id , amount , account_idx2);
+
 	} // End for loop
 
 	return NULL ;
 } // End teller thread method
 
 
+// Destroy mutexes
 void cleanup_mutexes () {
 	for ( int i = 0; i < NUM_ACCOUNTS; i++) {
 		pthread_mutex_destroy(&accounts[i].lock);
@@ -114,12 +100,14 @@ int main () {
 	// TODO 3: Add performance timing
 	// Reference : Section 7.2 " Performance Measurement "
 	// Hint : Use clock_gettime ( CLOCK_MONOTONIC , & start );
-	struct timespec start, end;
-	clock_gettime(CLOCK_MONOTONIC , &start);
+
+	struct timespec start, end;			// Create two timespec structs
+	clock_gettime(CLOCK_MONOTONIC , &start);	// Assign current time to start
+
+
 	printf ( "=== Phase 2: Race Conditions Demo ===\n\n" ) ;
-	// TODO 3 a : Initialize all accounts
-	// Hint : Loop through accounts array
-	// Set : account_id = i , balance = INITIAL_BALANCE , transaction_count = 0
+
+	// Initialize Accounts
 	for (int i = 0; i < NUM_ACCOUNTS; i++) {
 		accounts[i].account_id = i;
 		accounts[i].balance = INITIAL_BALANCE;
@@ -129,40 +117,26 @@ int main () {
 		pthread_mutex_init(&accounts[i].lock, NULL);
 	}
 
-	// Display initial state ( GIVEN )
+	// Display initial state
 	printf ("Initial State :\n" ) ;
-
 	for ( int i = 0; i < NUM_ACCOUNTS ; i ++) {
 		printf ("Account %d : $%.2f \n" , i , accounts[i].balance) ;
 	}
 
-	// TODO 3 b : Calculate expected final balance
-	// Question : With random deposits / withdrawals , what should total be ?
-	// Hint : Total money in system should remain constant !
+	double expected_total = NUM_ACCOUNTS * INITIAL_BALANCE;	// Set expected end balance (all transactions are transfers)
 
+	pthread_t threads[NUM_THREADS];	// Create array to hold threads
 
-	// TODO 3 c : Create thread and thread ID arrays
-	// Reference : man pthread_create for pthread_t type
+	int thread_ids[NUM_THREADS];	// Create array to hold thread ID's
 
-	pthread_t threads[NUM_THREADS];
-
-	int thread_ids[NUM_THREADS]; // GIVEN : Separate array for IDs
-
-	// TODO 3 d : Create all threads
-	// Reference : man pthread_create
-	// Caution : See Appendix A .2 warning about passing & i in loop !
-
+	// Create Threads
 	for ( int i = 0; i < NUM_THREADS ; i ++) {
-		thread_ids[i] = i ; // GIVEN : Store ID persistently
-		// YOUR pthread_create CODE HERE
-		// Format : pthread_create (& threads [ i ] , NULL , teller_thread , & thread_ids [ i ]) ;
+		thread_ids[i] = i ; // Store ID persistently
 		pthread_create(&threads[i], NULL, teller_thread, &thread_ids[i] );
 	}
 
-	// Reference : man pthread_join
-	// Question : What happens if you skip this step ?
+	// Join threads
 	for ( int i = 0; i < NUM_THREADS ; i ++) {
-		// YOUR pthread_join CODE HERE
 		pthread_join(threads[i], NULL);
 	}
 
@@ -171,11 +145,11 @@ int main () {
 	// Important : Destroy mutexes AFTER all threads complete !
 	cleanup_mutexes();
 
-	// TODO 3 f : Calculate and display results
 	printf ( "\n=== Final Results ===\n" ) ;
 
 	double actual_total = 0.0;
 
+	// Calculate and display final results
 	for (int i = 0; i < NUM_ACCOUNTS ; i ++) {
 
 		printf ("Account %d : $%.2f (%d transactions ) \n" ,
@@ -185,18 +159,23 @@ int main () {
 	printf ("\nExpected total: $%.2f \n" , expected_total ) ;
 	printf ("Actual total: $%.2f \n" , actual_total ) ;
 	printf ("Difference: $%.2f \n" , actual_total - expected_total ) ;
-	// TODO 3 g : Add race condition detection message
-	// If expected != actual , print " RACE CONDITION DETECTED !"
-	// Instruct user to run multiple times
+
+	// Test for Race Condition
 	if (actual_total != expected_total) {
-		printf("RACE CONDITION DETECTED!\n");
+		printf("\nRACE CONDITION DETECTED!\n");
 	}
 	else {
-		printf("NO RACE CONDITION DETECTED.\n");
+		printf("\nNO RACE CONDITION DETECTED.\n");
 	}
 
-	clock_gettime(CLOCK_MONOTONIC, &end);
-	double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) /1e9;
-	printf("Time: %.4f seconds\n", elapsed);
+	// TODO 3: Performance measurement
+
+	clock_gettime(CLOCK_MONOTONIC, &end);	// Set end to current time
+
+	// Calculate and display elapsed time in seconds
+	// End seconds - Start seconds + {(End nanoseconds - Start nanoseconds) converted to seconds}
+	double elapsed = (end.tv_sec - start.tv_sec) +( (end.tv_nsec - start.tv_nsec) /1e9 );
+	printf("\nTime: %.4f seconds\n", elapsed);
+
 	return 0;
 } // End Main
